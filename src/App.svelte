6be87681e390187d18892aba581a6f199f2461a9
Alexander Hardman <alexander.hardman@gmail.com>
2022-04-12 15:44:15 -0400

    successfully added theming!

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	export let name: string;
+	import { Accordion, AccordionItem } from "carbon-components-svelte";
+	import {
+    Theme,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";
+
+  let theme = "g10";
 </script>
 
 <main>
+	<Theme bind:theme />
+	<RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
+		{#each ["white", "g10", "g80", "g90", "g100"] as value}
+			<RadioButton labelText={value} {value} />
+		{/each}
+	</RadioButtonGroup>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
